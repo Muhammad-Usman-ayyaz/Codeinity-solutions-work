85c76b428ddc6f78f917ef1a711e3cda4b29bc3a
Muhammad usman <usmanayyazali@gmail.com>
2026-07-06 09:59:21 +0500

    Cv temepletate

---
+++ day 3/script.js
@@ -0,0 +1,8 @@
+btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    alert('Hello World');
+}); 
+btn2 = document.querySelector('button2');
+btn2.addEventListener('click', () => {
+    alert('Hello World');
+}); 
